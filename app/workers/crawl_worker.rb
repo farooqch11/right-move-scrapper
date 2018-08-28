cc03256d759f9bd8c416c02023d1335bdbce76d4
Farooq Ch <farooq.ashraf@tkxel.com>
2018-08-28 17:37:05 +0500

    updated ssl

--- app/workers/crawl_worker.rb
+++ app/workers/crawl_worker.rb
@@ -42,7 +42,7 @@ class CrawlWorker
 
         Capybara.register_driver :poltergeist do |app|
           Capybara::Poltergeist::Driver.new app,
-                                            phantomjs_options: ['--load-images=no','--ignore-ssl-errors=yes','--proxy-type=http',"--proxy=37.48.118.90:13040"],
+                                            phantomjs_options: ['--load-images=no','--ignore-ssl-errors=yes','--proxy-type=http','--ssl-protocol=any',"--proxy=37.48.118.90:13040"],
                                             js_errors: false,
                                             inspector: false,
                                             debug: false
